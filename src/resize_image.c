261644b6402e4b53c8073426bc9568f4d116fdad
aakankk <130693181+aakankk@users.noreply.github.com>
2023-07-16 21:03:34 +0530

    Update resize_image.c

--- src/resize_image.c
+++ src/resize_image.c
@@ -54,7 +54,24 @@ float bilinear_interpolate(image im, float x, float y, int c)
 
 image bilinear_resize(image im, int w, int h)
 {
-    
-    return make_image(1,1,1);
+    image newimage = make_image(w, h, im.c);
+
+    float width_scale = (float)im.w / w;
+    float height_scale = (float)im.h / h;
+
+    for(int k = 0; k < im.c; k++)
+        {
+            for(int j = 0; j < h; j++)
+                {
+                    for(int i=0; i < w; i++)
+                        {
+                            float a = (i + 0.5) * width_scale - 0.5;
+                            float b = (j + 0.5) * height_scale - 0.5;
+                            float temp = bilinear_interpolate(im, a, b, k);
+                            set_pixel(newimage, i, j, k, temp);
+                        }
+                }
+        }
+    return newimage;
 }
 
