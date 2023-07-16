0327e26b4daa317be731a06153cdc679ebd953de
aakankk <130693181+aakankk@users.noreply.github.com>
2023-07-16 20:04:50 +0530

    Update resize_image.c

--- src/resize_image.c
+++ src/resize_image.c
@@ -11,8 +11,25 @@ float nn_interpolate(image im, float x, float y, int a)
 
 image nn_resize(image im, int w, int h)
 {
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image newimage = make_image(w, h, im.c);
+
+    float width_scale = (float)im.w / w;
+    float height_scale = (float)im.h / h;
+
+    for (int k = 0; k < im.c; k++) 
+    {
+        for (int j = 0; j < h; j++) 
+        {
+            for (int i = 0; i < w; i++) 
+            {
+                float x = (i + 0.5) * width_scale - 0.5;
+                float y = (j + 0.5) * height_scale - 0.5;
+                float temp = nn_interpolate(im, x, y, k);
+                set_pixel(newimage, i, j, k, temp);
+            }
+        }
+    }
+    return newimage;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
